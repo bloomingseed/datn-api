e0c871dae3aeac474198e68c9d0b71f6f7671e7f
bloomingseed <nhattungnguyen.2kgl@gmail.com>
2022-06-16 22:13:40 +0700

    v1 - stand-alone

--- init.py
+++ init.py
@@ -6,16 +6,12 @@ import numpy as np
 # load test dataset
 ##
 dataset = pd.read_csv('./test.csv')
-words_series = dataset["text"].transform(lambda x : len(x.split()))
-boundaries = [100, 2000]
-lower_indicies = words_series[ words_series <= boundaries[0]].index.tolist()
-higher_indicies = words_series[ words_series >= boundaries[-1]].index.tolist()
-dataset = dataset.drop(lower_indicies + higher_indicies)
-dataset["words"] = words_series
 category_labels = ["Politics", "Business", "World", "Entertainment", "Sports", "Health", "Life", "Education", "Legal", "Tech"]
 source_labels = ["dantri.com.vn", "vnexpress.net", "thanhnien.vn", "vietnamnet.vn"]
+model_names = {'mnb': 'Multinomial Naive Bayes', 'svc': 'Linear Support Vector Classification', 'regression': 'Linear Regression', 'mlp': 'Multilayer Perceptron'}
+model_accuracies = {'mnb': 0.8628005657708628, 'svc': 0.9346220336319346, 'regression': 0.9137199434229137, 'mlp': 0.9371365707999372}
 ##
-# normalization module
+# text normalization module
 ##
 import string
 import re
@@ -40,7 +36,7 @@ def remove_url(text):
   pattern = r"https?://.+\S+"
   return re.sub(pattern, ' ', text)
 
-def normalization(text):
+def preprocess(text):
   text = lower_case(text)
   text = remove_url(text)
   text = remove_punctuations(text)
@@ -53,11 +49,62 @@ def normalization(text):
 # load pre-saved vectorizer, feature selector and models
 ##
 def load_model(name):
-    from joblib import load
-    return load('./pre-saved/' + name + '.joblib')
-    
+  from joblib import load
+  return load('./pre-saved/' + name + '.joblib')
+
 vectorizer, selector = load_model('vectorization'), load_model('feature_selection')
 my_models = {}
 for name in ['mnb', 'svc', 'regression', 'mlp']:
-    my_models[name] = load_model(name)
+  my_models[name] = load_model(name)
 
+##
+# Define pipeline
+##
+class ProcessPipeline:
+  def __init__(self, preprocessor, vectorizer, feature_selector):
+    self.preprocessor = preprocessor
+    self.tokenizer = vectorizer.build_analyzer()
+    self.vectorizer = vectorizer
+    self.selector = feature_selector
+      
+  def process(self, text, model = None):
+    self.preprocess = self.preprocessor(text)
+    self.tokenization = self.tokenizer(text)
+    self.vectorization_ = self.vectorizer.transform([self.preprocess])
+    self.feature_selection = self.selector.transform(self.vectorization_)
+    self.prediction = model.predict(self.feature_selection) if model != None else None
+    return self
+
+process_pipeline = ProcessPipeline(preprocess, vectorizer, selector)
+
+##
+# trimmer utility
+##
+def trim_middle_string(text, n = 50):
+    return text if len(text) <= 1.5 * n else '%s...hidden %d characters...%s' % (text[:n], len(text) - 2 * n, text[-n:])
+
+def arr_to_s(a):
+    return ', '.join([str(x) for x in a])
+
+def trim_middle_array(a, n = 50):
+    return arr_to_s(a) if len(a) <= 1.5 * n else arr_to_s(a[:50]) + '...hidden %d items...' % (len(a) - 2 * n) + arr_to_s(a[-50:])
+
+##
+# ISO date time string utility
+##
+def timestamp():
+    from datetime import datetime
+    return datetime.utcnow().strftime('%Y%m%dT%H%M%SZ')
+
+##
+# manual validation
+##
+class ManualValidator:
+  def __init__(self, dataset, pipeline):
+    self.dataset = dataset
+    self.pipeline = pipeline
+
+  def validate(self, model, n=100):
+    for i in range(n):
+      row = self.dataset.sample()
+      print(row.index.values[0], row.category_id.values[0], model.predict(self.pipeline.process(row.text.values[0]).feature_selection))
--- main.py
+++ main.py
@@ -1,29 +1,91 @@
-from init import dataset, category_labels, source_labels,vectorizer, selector, my_models
-from typing import Union
+import uvicorn
+from init import dataset, category_labels, source_labels, my_models, process_pipeline, trim_middle_string, trim_middle_array, model_names, model_accuracies, arr_to_s, timestamp
+from typing import List
 from fastapi import FastAPI
-
+from fastapi.responses import StreamingResponse, RedirectResponse, HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+import pandas as pd
+import io
 app = FastAPI()
+app.mount("/public", StaticFiles(directory="public"))
+app.add_middleware(CORSMiddleware, allow_origins = ["*"], allow_methods = ["*"],
+        allow_headers = ["*"], allow_credentials = True, expose_headers = ["Content-Disposition"])
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Model(BaseModel):
+    key: str
+    name: str
+    accuracy: float
 
+class Description(BaseModel):
+    length: int
+    models: List[Model]
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-    
-    
-@app.get("/dataset")
+class Row(BaseModel):
+    text: str
+    category: str
+    source: str
+    url: str
+
+class Entry(BaseModel):
+    text: str
+    model_name: str
+    export: bool = False
+
+class Result(BaseModel):
+    preprocess: str
+    tokenization: str
+    vectorization: str
+    prediction: str
+
+def iterfile(file_path):
+    with open(file_path, mode="rb") as file_like:
+        yield from file_like
+
+@app.get("/", response_class=HTMLResponse)
+def get_homepage():
+    with open('./index.html', mode="rb") as file:
+        return file.read()
+
+@app.get("/dataset", response_model = Description)
 def read_dataset():
-    return {'length': len(dataset)}
-    
-@app.get("/dataset/{index}")
+    return Description(
+            length = len(dataset),
+            models = [Model(key = key, name = model_names[key], accuracy = model_accuracies[key]) for key in model_names.keys()]
+            )
+
+@app.get("/dataset/{index}", response_model = Row)
 def read_dataset_record(index: int):
     row = dataset.loc[index]
-    return {
-        'text': row.text,
-        'category': category_labels[row.category_id],
-        'source': source_labels[row.source_id]
-    }
-    
+    return Row(
+        text = row.text,
+        category = category_labels[row.category_id],
+        source = source_labels[row.source_id],
+        url = row.url
+        )
+
+@app.post("/predict/", response_model = Result)
+def create_item(entry: Entry):
+    process_pipeline.process(entry.text, my_models[entry.model_name])
+    if entry.export:
+        df = pd.DataFrame({
+            'input': [entry.text], 'model': [entry.model_name],
+            'preprocess': [process_pipeline.preprocess],
+            'tokenization': [process_pipeline.tokenization],
+            'vectorization': [arr_to_s(process_pipeline.feature_selection.toarray()[0])],
+            'prediction': [category_labels[int(process_pipeline.prediction[0])]]
+            })
+        response = StreamingResponse(io.StringIO(df.to_csv(index=False)), media_type="text/csv")
+        response.headers['Content-Disposition'] = 'attachment; filename=prediction_%s.csv' % timestamp()
+        return response
+    else:
+        return Result(
+                preprocess = trim_middle_string(process_pipeline.preprocess, 200),
+                tokenization = trim_middle_array(process_pipeline.tokenization),
+                vectorization = trim_middle_array(process_pipeline.feature_selection.toarray()[0]),
+                prediction = category_labels[int(process_pipeline.prediction[0])]
+                )
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
